055f72765b7e452a534323a6df543555f6722d43
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-06 23:22:54 +0100

    add

--- learning ASYNC udemy/app.js
+++ learning ASYNC udemy/app.js
@@ -3,3 +3,6 @@ const multiply = (x, y) => x * y;
 const square = (x) => x * x;
 
 const isRightTriangle = (a, b, c) => square(a) + square(b) === square(c);
+
+isRightTriangle(3, 4, 5);
+//call-stack means adding breakpoint to our code

e68444dbf41b7e4af7c1e63d9e9a63971f05b52f
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-07 00:03:05 +0100

    avoid callback hell

--- learning ASYNC udemy/app.js
+++ learning ASYNC udemy/app.js
@@ -1,8 +1,31 @@
-const multiply = (x, y) => x * y;
+/* setTimeout(() => {
+  document.body.style.backgroundColor = "red";
+  setTimeout(() => {
+    document.body.style.backgroundColor = "blue";
 
-const square = (x) => x * x;
+    setTimeout(() => {
+      document.body.style.backgroundColor = "aqua";
+      setTimeout(() => {
+        document.body.style.backgroundColor = "pink";
+        setTimeout(() => {
+          document.body.style.backgroundColor = "purple";
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000); */
 
-const isRightTriangle = (a, b, c) => square(a) + square(b) === square(c);
+const color = function (paint, time, doNext) {
+  setTimeout(() => {
+    document.body.style.backgroundColor = paint;
+    doNext();
+  }, time);
+};
 
-isRightTriangle(3, 4, 5);
-//call-stack means adding breakpoint to our code
+color("red", 1000, () => {
+  color("pink", 1000, () => {
+    color("red", 1000, () => {
+      color("pink", 1000, () => {});
+    });
+  });
+});

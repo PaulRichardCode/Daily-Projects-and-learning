e42031b514f3165613ca8c222926d6b222a21bc3
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2022-12-10 14:31:02 +0100

    codimg is hard

--- navbar/app.js
+++ navbar/app.js
@@ -5,16 +5,13 @@
 // toggle - toggles class
 
 const navToggle = document.querySelector(".nav-toggle");
+
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
-  console.log(links.classList);
-  console.log(links.classList.contains("random"));
-  console.log(links.classList.contains("links"));
-  if (links.classList.contains("show-links")) {
-    links.classList.remove("show-links");
-  } else {
-    links.classList.add("show-links");
+navToggle.addEventListener("click", () => {
+  const visibility = links.getAttribute("data-visible");
+
+  if (visibility === false) {
+    links.setAttribute("data-visible", true);
   }
-  links.classList.toggle("show-links");
 });
